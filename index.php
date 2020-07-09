600e7b6962d3cf949920225038f0c3f0fcf86ac0
rafael <rafaelhss@gmail.com>
2020-07-09 14:39:52 -0300

    v1

--- index.php
+++ index.php
@@ -1,11 +1,2 @@
-<?php
-
-if($_SERVER['HTTPS']!="on") {
-
-$redirect= "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-
-header("Location:$redirect"); }
-
-?>
 
 <?php include_once("index.html"); ?>
