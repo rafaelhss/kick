a284226df5b79f266082f0b796058bebf49f4064
rafael <rafaelhss@gmail.com>
2020-07-09 14:17:19 -0300

    v1

---
+++ js/pitchdetect.js
@@ -0,0 +1,227 @@
+
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+var audioContext = null;
+var isPlaying = false;
+var sourceNode = null;
+var analyser = null;
+var theBuffer = null;
+var DEBUGCANVAS = null;
+var mediaStreamSource = null;
+var detectorElem, 
+	canvasElem,
+	waveCanvas,
+	pitchElem,
+	noteElem,
+	detuneElem,
+	detuneAmount;
+
+window.onload = function() {
+	audioContext = new AudioContext();
+	MAX_SIZE = Math.max(4,Math.floor(audioContext.sampleRate/5000));	// corresponds to a 5kHz signal
+	var request = new XMLHttpRequest();
+	request.open("GET", "../sounds/whistling3.ogg", true);
+	request.responseType = "arraybuffer";
+	request.onload = function() {
+	  audioContext.decodeAudioData( request.response, function(buffer) { 
+	    	theBuffer = buffer;
+		} );
+	}
+	request.send();
+
+	detectorElem = document.getElementById( "detector" );
+	canvasElem = document.getElementById( "output" );
+	DEBUGCANVAS = document.getElementById( "waveform" );
+	if (DEBUGCANVAS) {
+		waveCanvas = DEBUGCANVAS.getContext("2d");
+		waveCanvas.strokeStyle = "black";
+		waveCanvas.lineWidth = 1;
+	}
+	pitchElem = document.getElementById( "pitch" );
+	noteElem = document.getElementById( "note" );
+	detuneElem = document.getElementById( "detune" );
+	detuneAmount = document.getElementById( "detune_amt" );
+
+	detectorElem.ondragenter = function () { 
+		this.classList.add("droptarget"); 
+		return false; };
+	detectorElem.ondragleave = function () { this.classList.remove("droptarget"); return false; };
+	detectorElem.ondrop = function (e) {
+  		this.classList.remove("droptarget");
+  		e.preventDefault();
+		theBuffer = null;
+
+	  	var reader = new FileReader();
+	  	reader.onload = function (event) {
+	  		audioContext.decodeAudioData( event.target.result, function(buffer) {
+	    		theBuffer = buffer;
+	  		}, function(){alert("error loading!");} ); 
+
+	  	};
+	  	reader.onerror = function (event) {
+	  		alert("Error: " + reader.error );
+		};
+	  	reader.readAsArrayBuffer(e.dataTransfer.files[0]);
+	  	return false;
+	};
+
+
+
+}
+
+function error() {
+    alert('Stream generation failed.');
+}
+
+function getUserMedia(dictionary, callback) {
+    try {
+        navigator.getUserMedia = 
+        	navigator.getUserMedia ||
+        	navigator.webkitGetUserMedia ||
+        	navigator.mozGetUserMedia;
+        navigator.getUserMedia(dictionary, callback, error);
+    } catch (e) {
+        alert('getUserMedia threw exception :' + e);
+    }
+}
+
+function gotStream(stream) {
+    // Create an AudioNode from the stream.
+    mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+    // Connect it to the destination.
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    mediaStreamSource.connect( analyser );
+    updatePitch();
+}
+
+
+
+function toggleLiveInput() {
+    if (isPlaying) {
+        //stop playing and return
+        sourceNode.stop( 0 );
+        sourceNode = null;
+        analyser = null;
+        isPlaying = false;
+		if (!window.cancelAnimationFrame)
+			window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
+        window.cancelAnimationFrame( rafID );
+        
+        document.getElementById("button").innerHTML = "Retomar";
+        
+    } else {
+        document.getElementById("button").innerHTML= "Pausar"
+
+
+        sourceNode = audioContext.createBufferSource();
+        sourceNode.buffer = theBuffer;
+        sourceNode.loop = true;
+
+        analyser = audioContext.createAnalyser();
+        analyser.fftSize = 2048;
+        sourceNode.connect( analyser );
+        analyser.connect( audioContext.destination );
+        sourceNode.start( 0 );
+        isPlaying = true;
+        isLiveInput = false;
+        updatePitch();
+
+        getUserMedia(
+            {
+                "audio": {
+                    "mandatory": {
+                        "googEchoCancellation": "false",
+                        "googAutoGainControl": "false",
+                        "googNoiseSuppression": "false",
+                        "googHighpassFilter": "false"
+                    },
+                    "optional": []
+                },
+            }, gotStream);
+    }
+}
+
+
+
+var rafID = null;
+var tracks = null;
+var buflen = 1024;
+var buf = new Float32Array( buflen );
+
+
+var countZeros = 0;
+var countNonZeros = 0;
+var countKick = 0;
+
+
+function draw(){
+    if (DEBUGCANVAS) {  // This draws the current waveform, useful for debugging
+		waveCanvas.clearRect(0,0,512,256);
+		/*waveCanvas.strokeStyle = "red";
+		waveCanvas.beginPath();
+		waveCanvas.moveTo(0,0);
+		waveCanvas.lineTo(0,256);
+		waveCanvas.moveTo(128,0);
+		waveCanvas.lineTo(128,256);
+		waveCanvas.moveTo(256,0);
+		waveCanvas.lineTo(256,256);
+		waveCanvas.moveTo(384,0);
+		waveCanvas.lineTo(384,256);
+		waveCanvas.moveTo(512,0);
+		waveCanvas.lineTo(512,256);
+		waveCanvas.stroke();*/
+		waveCanvas.strokeStyle = "black";
+		waveCanvas.beginPath();
+		waveCanvas.moveTo(0,128);
+		for (var i=1;i<512;i++) {
+			waveCanvas.lineTo(i,128+(buf[i]*128));
+		}
+		waveCanvas.stroke();
+	}
+}
+
+function updatePitch( time ) {
+	analyser.getFloatTimeDomainData( buf );
+	draw();
+    var countBuffkick = 0;
+    
+    for (var i=1;i<512;i++) {
+        if(buf[i] > 1.005){
+            countBuffkick +=1;
+        }
+    }
+
+    if(countBuffkick == 0){
+        countZeros++;
+        if(countNonZeros == 0 && countZeros == 20){ //contar chute
+            countKick++;
+            console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@countKick: " + countKick)
+            document.getElementById("countKick").innerHTML = countKick;
+            var pcent = Math.round((countKick / 1000) * 100);
+            document.getElementById("progressdone").style.width = pcent+"%";
+            document.getElementById("progressdone").innerHTML = pcent + " %";
+            
+            if(countKick % 10 == 0){
+                var su = new SpeechSynthesisUtterance();
+                su.lang = "pt-BR";
+                su.text = countKick;
+                speechSynthesis.speak(su);
+            }
+        } 
+
+        countNonZeros = 0;
+    } else {
+        countZeros = 0;
+        countNonZeros++; 
+    }
+     console.log("countBuffkick: " + countBuffkick + "   countNonZeros: " + countNonZeros + "   countZeros:" + countZeros + "countKick: " + countKick);
+
+    
+
+	if (!window.requestAnimationFrame)
+		window.requestAnimationFrame = window.webkitRequestAnimationFrame;
+	rafID = window.requestAnimationFrame( updatePitch );
+}
